d4cfe12c247fd6f6bd8717c1a41ca094783e7c67
Venkatramanan Balakrishnan <venkatramanan.balakrishnan@in.abb.com>
2017-11-17 16:01:15 +0530

    updates to About Us page

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -13,17 +13,17 @@ export class DashboardComponent implements OnInit {
 
     constructor() {
         this.sliders.push({
-            imagePath: 'assets/images/slider1.jpg',
-            label: 'First slide label',
-            text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+            imagePath: 'assets/img/Ramaphotos/rama2.jpg',
+            label: 'Rama Rama',
+            text: 'hare rama hare rama rama rama hare hare'
         }, {
-            imagePath: 'assets/images/slider2.jpg',
-            label: 'Second slide label',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            imagePath: 'assets/img/Ramaphotos/rama3.jpg',
+            label: 'Rama Rama',
+            text: 'hare rama hare rama rama rama hare hare'
         }, {
-            imagePath: 'assets/images/slider3.jpg',
-            label: 'Third slide label',
-            text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+            imagePath: 'assets/img/Ramaphotos/rama4.jpg',
+            label: 'Rama Rama',
+            text: 'hare rama hare rama rama rama hare hare'
         });
 
         this.alerts.push({

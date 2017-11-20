14958a65256ec5d4b511e84a8ee01b1c16bbc21f
Venkatramanan Balakrishnan <venkatramanan.balakrishnan@in.abb.com>
2017-11-20 14:14:11 +0530

    updates changes for photo page

--- src/app/layout/form/form.component.ts
+++ src/app/layout/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-form',
@@ -8,6 +10,23 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class FormComponent implements OnInit {
-    constructor() { }
+
+    albumName = ['Event 2015',
+                'Event 2016',
+                'Event 2017',
+                'Event 2018'];
+
+imgUrl = [`./assets/img/Ramaphotos/rama5.jpg`,
+            `./assets/img/Ramaphotos/rama2.jpg`,
+            `./assets/img/Ramaphotos/rama3.jpg`,
+            `./assets/img/Ramaphotos/rama4.jpg`];
+
+    constructor(private router: Router, private route: ActivatedRoute) { }
     ngOnInit() {}
+
+    onclk(eventName){
+
+        this.router.navigate(['../blank-page',{AlbumName:eventName}], { relativeTo: this.route });
+
+    }
 }
